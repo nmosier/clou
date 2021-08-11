00c916f3717e7a1825a8ff9d55ccac0b253bd89d
Nicholas Mosier <nh.mosier@gmail.com>
2021-08-11 08:39:33 -0700

    initial commit

---
+++ src/xmacro.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef STR
+# define STR(x) #x
+#endif
+
+#ifndef XSTR
+# define XSTR(x) STR(x)
+#endif
+
+#define XM_ENUM_DEF_ELT(x) x,
+#define XM_ENUM_DEF(name, XM)                   \
+   enum name {                                  \
+      XM(XM_ENUM_DEF_ELT)                       \
+   };
+
+#define XM_ENUM_TOSTR_ELT(x) case x: return XSTR(x);
+#define XM_ENUM_TOSTR(XM, var, dfl)                 \
+   switch (var) {                                   \
+      XM(XM_ENUM_TOSTR_ELT)                         \
+   default: return dfl;                             \
+   }
