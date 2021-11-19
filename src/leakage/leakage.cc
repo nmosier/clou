08df875ed9192e3978ddcc6b29c0b3300c1cfc40
Nicholas Mosier <nmosier@stanford.edu>
2021-11-19 08:18:26 -0800

    add status update

--- src/leakage/leakage.cc
+++ src/leakage/leakage.cc
@@ -457,9 +457,11 @@ void Detector::for_one_transmitter(NodeRef transmitter, std::function<void (Node
             }
         }
     }
-    
+
+    std::optional<Timer> timer_opt = Timer();
     z3_scope;
     for (const z3::expr& e : vec) { solver.add(e); }
+    timer_opt = std::nullopt;
     
     std::cerr << "Assertions: " << solver.assertions() << "\n";
     if (solver.check() != z3::unsat) {
