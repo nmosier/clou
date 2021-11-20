0385c1299dcdb623354b725d689f80d0089b7df6
Nicholas Mosier <nmosier@stanford.edu>
2021-11-19 16:27:38 -0800

    initial commit

---
+++ docker-run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eu
+
+usage() {
+    cat <<EOF
+usage: $0 <name>
+EOF
+}
+
+if [[ $# -ne 1 ]]; then
+    usage >&2
+    exit 1
+fi
+
+NAME="$1"
+
+docker run -it --privileged --cap-add=SYS_PTRACE --security-opt=seccomp=unconfined "$NAME"
