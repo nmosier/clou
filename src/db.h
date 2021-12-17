4f1511b94573b35da13909293de790965f3bdd35
Nicholas Mosier <nmosier@stanford.edu>
2021-12-17 10:41:25 -0700

    use coarser lock for db

--- src/db.h
+++ src/db.h
@@ -49,6 +49,48 @@ private:
     }
 };
 
+namespace gdbm {
+
+class File {
+public:
+    File() {}
+    File(const std::string& path, int flags, int mode = 0) {
+        open(path, flags, mode);
+    }
+    
+    ~File() {
+        if (good()) {
+            close();
+        }
+    }
+    
+    bool good() const { return f != nullptr; }
+    operator bool() const { return good(); }
+    
+    void open(const std::string& path, int flags, int mode = 0) {
+        if ((f = ::gdbm_open(path.c_str(), 0, flags, mode, nullptr)) == nullptr) {
+            throw gdbm_exception("gdbm_open");
+        }
+    }
+    
+    void close() {
+        if (good()) {
+            if (::gdbm_close(f) < 0) {
+                throw gdbm_exception("gdbm_close");
+            }
+            f = nullptr;
+        }
+    }
+    
+    operator GDBM_FILE() const { return f; }
+    
+private:
+    GDBM_FILE f = nullptr;
+};
+
+}
+
+#if 0
 class SharedDatabase {
 public:
     SharedDatabase() {}
@@ -124,3 +166,61 @@ private:
         return FileMutex(fd(), operation);
     }
 };
+#else
+class SharedDatabase {
+public:
+    SharedDatabase() {}
+    
+    bool good() const { return path.empty(); }
+    operator bool() const { return good(); }
+    
+    void open(const std::string& path) {
+        this->path = path;
+    }
+    
+    void close() {
+        path.clear();
+    }
+    
+    bool contains(const std::string& key) const {
+        gdbm::File f = file();
+        datum d = {
+            .dptr = const_cast<char *>(key.data()),
+            .dsize = static_cast<int>(key.size()),
+        };
+        const datum ent = gdbm_fetch(f, d);
+        if (ent.dptr == nullptr) {
+            if (gdbm_errno == GDBM_ITEM_NOT_FOUND) {
+                return false;
+            } else {
+                throw gdbm_exception("gdbm_fetch");
+            }
+        } else {
+            return true;
+        }
+    }
+    
+    void insert(const std::string& key) {
+        gdbm::File f = file();
+        const datum k = {
+            .dptr = const_cast<char *>(key.data()),
+            .dsize = static_cast<int>(key.size()),
+        };
+        if (gdbm_store(f, k, k, 0) == -1) {
+            throw gdbm_exception("gdbm_store");
+        }
+        if (gdbm_sync(f) < 0) {
+            throw gdbm_exception("gdbm_sync");
+        }
+    }
+    
+private:
+    std::string path;
+    
+    gdbm::File file() const {
+        return gdbm::File(path, GDBM_WRCREAT, 0664);
+    }
+};
+
+
+#endif
