d698e8b4ad1b0b0b3d706878982275247f693717
Nicholas Mosier <nmosier@stanford.edu>
2022-06-11 08:13:31 -0700

    update

--- scripts/transmitters.sh
+++ scripts/transmitters.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-UNIQLC="$(basename ${BASH_SOURCE[0]})/uniqlc"
+UNIQLC="$(dirname ${BASH_SOURCE[0]})/uniqlc"
 
 for LKG in $(find "$@" -type f); do
     head -50 "$LKG" | grep '^[[:digit:]]\+: \(LOAD\|STORE\)' | tail -1 | tr -s ' ' | cut -d' ' -f3-
