4c86c4f56a23fa8be7fabc30b9c98eb361011b7a
Nicholas Mosier <nmosier@stanford.edu>
2022-06-15 19:30:53 -0700

    optional command-line option for transmitter type

--- scripts/run-bench.sh
+++ scripts/run-bench.sh
@@ -11,14 +11,13 @@ Options:
   -t v1|v4       type of Spectre leakage to detect [required]:
      	    	   - v1: Spectre v1 leakage
 		   - v4: Spectre v4 leakage
-  -p <pattern>   transmitter pattern to look for [optional, default: "addr_gep,addr"]:
+  -x <xmitter>   transmitter type to look for [optional, default=udt]:
+     		   - udt: universal data transmitters    -> sets pattern "addr_gep,addr"
+		   - uct: unviersal control transmitters -> sets pattern "addr_gep,ctrl"
+		   - dt:  data transmitters 		 -> sets pattern "addr"
+		   - ct:  control transmitters 		 -> sets pattern "ctrl"
+  -p <pattern>   transmitter pattern to look for [optional, default: set by transmitter type "-x"]
      		   General form, as regexp: ((addr|addr_gep|ctrl),)*(addr|addr_gep|ctrl)
-		   Commonly used patterns:
-     		   - addr_gep,addr -- subset of universal data leakage that feature array accesses
-		   - addr,addr     -- all universal data leakage
-		   - ctrl,addr     -- universal control leakage
-		   - addr  	   -- plain data leakage
-		   - ctrl	   -- plain control leakage
   -j <jobs>      number of parallel compilation jobs [optional, default: 1]
   -J <jobs>      max number of threads across all compilation jobs [optional, default: 1]
   -T <file-to>   file timeout, in seconds [optional, default: 3600]
@@ -35,7 +34,8 @@ JOBS=1
 THREADS=1
 SPECTRE_TYPE=
 FILE_TIMEOUT=3600
-while getopts "ht:p:j:J:T:O:" OPTC; do
+TRANSMITTER=udt
+while getopts "ht:p:j:J:T:O:x:" OPTC; do
       case $OPTC in
 	  h)
 	      usage
@@ -59,6 +59,9 @@ while getopts "ht:p:j:J:T:O:" OPTC; do
 	  O)
 	      CLOU_OUT="$OPTARG"
 	      ;;
+	  x)
+	      TRANSMITTER="$OPTARG"
+	      ;;
 	  *)
 	      usage >&2
 	      exit 1
@@ -77,6 +80,28 @@ fi
 if [[ $# -lt 1 ]]; then
     echo "$0: missing positional argument <benchmark>" >&2
     exit 1
+fi
+
+# set pattern
+if [[ -z "${PATTERN+a}" ]]; then
+    case "$TRANSMITTER" in
+	udt)
+	    PATTERN="addr_gep,addr"
+	    ;;
+	uct)
+	    PATTERN="addr_gep,ctrl"
+	    ;;
+	dt)
+	    PATTERN="addr"
+	    ;;
+	ct)
+	    PATTERN="ctrl"
+	    ;;
+	*)
+	    echo "$0: invalid transmitter type \"$TRANSMITTER\"" >&2
+	    exit 1
+	    ;;
+    esac
 fi
 
 BENCH="$1"
@@ -126,7 +151,7 @@ mkdir $CLOU_OUT
 export CLOU_OUT
 export THREADS
 . "${LCM_ARGS_FILE}"
-export LCM_ARGS="$(get_lcm_args "$SPECTRE_TYPE") -o $CLOU_OUT --file-timeout=$FILE_TIMEOUT $@"
+export LCM_ARGS="$(get_lcm_args "$SPECTRE_TYPE") -o $CLOU_OUT --file-timeout=$FILE_TIMEOUT --deps=$PATTERN $@"
 
 WIN="$BENCH"
 MONITOR=monitor
